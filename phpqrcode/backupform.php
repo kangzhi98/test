4f6e0c781c6e4fa7f61560148282de5b1cf6bc01
kangzhi98 <56619456+kangzhi98@users.noreply.github.com>
2022-12-10 13:21:57 +0800

    Add files via upload

---
+++ phpqrcode/backupform.php
@@ -0,0 +1,54 @@
+
+
+<form action="codeprocess.php" method="post">
+    <label>Name :</label>
+    <input type="text" name="name" />
+<br/><br/>
+    <label>Sort Name </label>
+    <input type="text" name="sortname" />
+    <br/><br/>
+    <label>Phone :</label>
+    <input type="" name="phone" />
+    <br/><br/>
+    <label>Phone Private :</label>
+    <input type="text" name="phoneprivate" />
+    <br/><br/>
+    <label>Phone Cell :</label>
+    <input type="text" name="phonecell" />
+    <br/><br/>
+    <label>Organization :</label>
+    <input type="text" name="orgname" />
+    <br/><br/>
+    <label>Email :</label>
+    <input type="email" name="email" />
+    <br/><br/>
+    <label>Address Label :</label>
+    <input type="text" name="addresslabel" />
+    <br/><br/>
+    <label>Address Pobox :</label>
+    <input type="text" name="addresspobox" />
+    <br/><br/>
+    <label>Address Ext :</label>
+    <input type="text" name="addressext" />
+    <br/><br/>
+    <label>Address Street :</label>
+    <input type="text" name="addressstreet" />
+    <br/><br/>
+    <label>Address Town :</label>
+    <input type="text" name="addresstown" />
+    <br/><br/>
+    <label>Address Region :</label>
+    <input type="text" name="addressregion" />
+    <br/><br/>
+    <label>Address Post Code :</label>
+    <input type="text" name="addresspostcode" />
+    <br/><br/>
+    <label>Address Country :</label>
+    <input type="text" name="addresscountry" />
+
+    <br/><br/>
+    <input type="submit" value="Generate">
+
+    
+</form>
+<div id="result"></div>
---
+++ phpqrcode/rawform.php
@@ -0,0 +1,49 @@
+<form action="generate-contact.php" method="post">
+    <label>Name :</label>
+    <input type="text" name="name" />
+
+    <label>Sort Name </label>
+    <input type="text" name="sortname" />
+
+    <label>Phone :</label>
+    <input type="" name="phone" />
+
+    <label>Phone Private :</label>
+    <input type="text" name="phoneprivate" />
+
+    <label>Phone Cell :</label>
+    <input type="text" name="phonecell" />
+
+    <label>Organization :</label>
+    <input type="text" name="orgname" />
+
+    <label>Email :</label>
+    <input type="email" name="email" />
+
+    <label>Address Label :</label>
+    <input type="text" name="addresslabel" />
+
+    <label>Address Pobox :</label>
+    <input type="text" name="addresspobox" />
+
+    <label>Address Ext :</label>
+    <input type="text" name="addressext" />
+
+    <label>Address Street :</label>
+    <input type="text" name="addressstreet" />
+
+    <label>Address Town :</label>
+    <input type="text" name="addresstown" />
+
+    <label>Address Region :</label>
+    <input type="text" name="addressregion" />
+
+    <label>Address Post Code :</label>
+    <input type="text" name="addresspostcode" />
+
+    <label>Address Country :</label>
+    <input type="text" name="addresscountry" />
+
+
+    
+</form>
